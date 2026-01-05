538325d9f313404d58f1e82d102464b3c2b6a3db
Adin Piric <adinpiric18@gmail.com>
2026-01-05 16:08:46 +0100

    Register radi

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,8 +1,12 @@
 package controller;
 
+import database.DatabaseConnection;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Alert;
 
 public class LoginController {
 
@@ -10,12 +14,53 @@ public class LoginController {
     @FXML private PasswordField passwordField;
 
     @FXML
-    public void handleLogin() {
-        System.out.println("Kliknut Login: " + usernameField.getText());
+    public void handleRegister() {
+        String user = usernameField.getText();
+        String pass = passwordField.getText();
+
+
+        if (user.isEmpty() || pass.isEmpty()) {
+            prikaziAlert("Greška", "Sva polja moraju biti popunjena!");
+            return;
+        }
+
+        try {
+
+            MongoCollection<Document> users = DatabaseConnection.getDatabase().getCollection("users");
+
+
+            Document postojeciKorisnik = users.find(new Document("username", user)).first();
+            if (postojeciKorisnik != null) {
+                prikaziAlert("Greška", "Korisničko ime je već zauzeto!");
+                return;
+            }
+
+
+            Document newUser = new Document("username", user).append("password", pass);
+            users.insertOne(newUser);
+
+            prikaziAlert("Uspjeh", "Uspješno ste se registrovali!");
+
+
+            usernameField.clear();
+            passwordField.clear();
+
+        } catch (Exception e) {
+            prikaziAlert("Baza podataka", "Greška pri povezivanju: " + e.getMessage());
+        }
     }
 
     @FXML
-    public void handleRegister() {
-        System.out.println("Kliknut Register: " + usernameField.getText());
+    public void handleLogin() {
+
+        System.out.println("Kliknut login.");
+    }
+
+    private void prikaziAlert(String naslov, String sadrzaj) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(naslov);
+        alert.setHeaderText(null);
+        alert.setContentText(sadrzaj);
+        alert.showAndWait();
     }
 }
