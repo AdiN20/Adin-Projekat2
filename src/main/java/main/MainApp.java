9aeff1f60f9fca0ec6ef8e7f2cbf826cf938554d
Adin Piric <adinpiric18@gmail.com>
2026-01-05 15:58:42 +0100

    first commit

--- src/main/java/main/MainApp.java
+++ src/main/java/main/MainApp.java
@@ -10,19 +10,19 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Putanja do tvog FXML fajla (pazi da je folder view unutar resources)
+
         Parent root = FXMLLoader.load(getClass().getResource("/view/login.fxml"));
 
-        // Postavljamo naslov i veličinu prozora
+
         primaryStage.setTitle("Life Management System - Login");
         primaryStage.setScene(new Scene(root, 400, 400));
 
-        // Prikazujemo prozor
+
         primaryStage.show();
     }
 
     public static void main(String[] args) {
-        // Pokreće JavaFX aplikaciju
+
         launch(args);
     }
 }
